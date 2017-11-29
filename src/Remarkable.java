d88cacc333218c00d27dd6efb773fbd37db05e83
timperi.emil <timperi.emil@SEMS-259-09.SEMS259.LAB>
2017-11-29 14:55:19 -0800

    Unbirthday, remarkable ip

---
+++ src/Remarkable.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {public static void main(String[] args) {
+	String name = JOptionPane.showInputDialog("Whats your name person.");
+	if(name.equalsIgnoreCase("Callum")) {
+		JOptionPane.showMessageDialog(null, "You have the best inspirational quotes.");
+	}
+	else if(name.equalsIgnoreCase("Lauren")) { JOptionPane.showMessageDialog(null, "You are an amazing person conratulations!");
+	else if(name.equalsIgnoreCase("Victor")) { JOptionPane.showMessageDialog(null, "Spicy Glasses");
+}
+}
+}
---
+++ src/Unbirthday.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {public static void main(String[] args) {
+	String birthday = JOptionPane.showInputDialog("What is Your birthday");
+	if(birthday.equals("11/29")) {
+		JOptionPane.showMessageDialog(null, "Happy Birthday");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+}
+}
+}
