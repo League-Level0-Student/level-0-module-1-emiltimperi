117405b1cbfed7abc72e1e1266732338cd0a495c
timperi.emil <timperi.emil@SEMS-259-08.SEMS259.LAB>
2018-01-17 14:44:15 -0800

    Badgers

---
+++ src/Badgers.java
@@ -0,0 +1,23 @@
+
+public class Badgers {
+public static void main(String[] args) {
+	for (int i = 0; i < 3; i++) {
+		
+		for (int j = 0; j < 10; j++) {
+			System.out.print("Badger, ");
+		} System.out.println();
+		for (int j = 0; j < 2; j++) {
+			System.out.print("Mushroom, ");
+			
+			
+		} System.out.println();
+		if (i == 2) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print("snake, ");
+				
+			}
+	}
+		
+	}
+}
+}
